73d588d1d16a4e5603e0233f18ff8a9d26417b37
hcweigand10 <henryweigand10@gmail.com>
2023-02-24 14:29:26 -0800

    remove skyfall

--- src/components/sections/Projects.js
+++ src/components/sections/Projects.js
@@ -1,20 +1,37 @@
-import React from 'react'
-import ProjectPreview from '../ProjectPreview';
-import projects from '../../utils/projectsData';
+import React from "react";
+import ProjectPreview from "../ProjectPreview";
+import projects from "../../utils/projectsData";
 
 const Projects = () => {
-
-    const previews = [projects.doschRealty, projects.browserParty, projects.geoquizzer, projects.peterMusic, projects.skyfallTravel].map((project, index) => <ProjectPreview thumbnail={project.thumbnail} hook={project.hook} title={project.title} liveUrl={project.liveUrl} key={index}/>)
-
+  const previews = [
+    projects.doschRealty,
+    projects.browserParty,
+    projects.geoquizzer,
+    projects.peterMusic,
+  ].map((project, index) => {
+    console.log(project.title);
     return (
-        <div className='py-4' id='projects'>
-             <h2 className='text-center section-heading mb-1'>Projects</h2>
-             <div className='d-flex justify-content-center'><hr className='section-underline underline-primary'/></div>
-             <div className='mx-auto' style={{width: "90vw", maxWidth: "1000px"}}>    
-                {previews}
-             </div>
-        </div>
-    )
-}
+      <ProjectPreview
+        thumbnail={project.thumbnail}
+        hook={project.hook}
+        title={project.title}
+        liveUrl={project.liveUrl}
+        key={index}
+      />
+    );
+  });
 
-export default Projects
+  return (
+    <div className="py-4" id="projects">
+      <h2 className="text-center section-heading mb-1">Projects</h2>
+      <div className="d-flex justify-content-center">
+        <hr className="section-underline underline-primary" />
+      </div>
+      <div className="mx-auto" style={{ width: "90vw", maxWidth: "1000px" }}>
+        {previews}
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
